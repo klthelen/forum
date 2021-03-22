225e5d0ad48fcf8121d9169536ca2d5767f64de6
Kirk Thelen <k47542637@gmail.com>
2021-03-22 17:04:44 -0400

    Refactoring & organization

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
-import Home from './Home';
+import Home from './pages/Home';
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import About from './pages/About';
+import Rules from './pages/Rules';
+import FAQ from './pages/FAQ';
+import DoesNotExist from './pages/DoesNotExist';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/About">
+              <About />
+            </Route >
+            <Route exact path="/FAQ">
+              <FAQ />
+            </Route >
+            <Route exact path="/Rules">
+              <Rules />
+            </Route >
+            <Route path="*">
+              <DoesNotExist />
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
